c0e6d9ed046adf53a2d205ce44b2cd6df897715c
Neil Okamoto <neil.okamoto@gmail.com>
2017-01-26 12:17:47 -0800

    updated to parse the RSS quote feed directly rather than scrape the site

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [hickory "0.6.0"]
-                 [clj-http "3.1.0"]]
+                 [org.clojars.scsibug/feedparser-clj "0.4.0"]
+                 [clj-http "3.4.1"]]
   :main adafruit-wisdom.core)
--- src/adafruit_wisdom/core.clj
+++ src/adafruit_wisdom/core.clj
@@ -1,30 +1,18 @@
 (ns adafruit-wisdom.core
  (:require [clj-http.client :as client]
-           [hickory.select :as s]
-           [hickory.core :as h])
+           [feedparser-clj.core :refer [parse-feed]])
  (:gen-class))
-
 
 (defn -main
   [& args]
-  (let [fruit (-> (client/get "http://adafruit.com")
+  (let [fruit (-> (client/get "http://adafruit.com/feed/quotes.xml")
                   :body
-                  h/parse
-                  h/as-hickory)
-        quote (-> (s/select (s/and (s/tag :span)
-                                   (s/or (s/class "quotes-large")
-                                         (s/class "quotes-medium")))
-                            fruit)
-                  first
-                  :content
-                  first)
-        authr (-> (s/select (s/child (s/class "quotes_author")
-                                     (s/tag :a))
-                            fruit)
-                  first
-                  :content
-                  first)]
-    (println (str quote " - " authr))))
+                  .getBytes
+                  java.io.ByteArrayInputStream.)
+        quote (-> fruit
+                  parse-feed
+                  :entries
+                  rand-nth
+                  :title)]
+    (println quote)))
 
-
-;; (-main)
